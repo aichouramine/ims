895741d297f17ff7056a47df93ee3e2d2c8890c4
Dzmitry Ihnatovich <ignatovich.dm@gmail.com>
2019-04-18 13:35:53 +0300

    Added QA Needs view

--- src/components/NeedsView/NeedsView.js
+++ src/components/NeedsView/NeedsView.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import classes from './NeedsView.module.css';
 import BenchTable from './BenchTable/BenchTable';
+import ProjectsTable from './ProjectsTable/ProjectsTable';
+import MasteryTable from './MasteryTable/MasteryTable';
+import Hoc from '../../hoc/Hoc';
 
 const needsView = () => (
-    <div className="row">
-        <div className="mb-4 col-sm-12 col-md-8 col-lg-8">
-
-        </div>
-        <div className={`${classes.Card} card-small mb-4 col-sm-12 col-md-4 col-lg-4`}>
-            <div className={`${classes.Card_header} card-header border-bottom`}>
-                <h6 className="m-0">Bench</h6>
+    <Hoc>
+        <div className="row">
+            <div className="mb-4 col-sm-12 col-md-12 col-lg-12 ">
+                <div className={`${classes.Card} card-small `}>
+                    <div className={`${classes.Card_header} card-header border-bottom`}>
+                        <h6 className="m-0">Project Needs</h6>
+                    </div>
+                    <div className="card-body p-0 pb-3 text-center">
+                        <ProjectsTable/>
+                    </div>
+                </div>
             </div>
-            <div className="card-body p-0 pb-3 text-center">
-                <BenchTable/>
+        </div>
+        <div className="row">
+            <div className="mb-4 col-sm-12 col-md-6 col-lg-6 ">
+                <div className={`${classes.Card} card-small `}>
+                    <div className={`${classes.Card_header} card-header border-bottom`}>
+                        <h6 className="m-0">Bench</h6>
+                    </div>
+                    <div className="card-body p-0 pb-3 text-center">
+                        <BenchTable/>
+                    </div>
+                </div>
+            </div>
+            <div className="mb-4 col-sm-12 col-md-6 col-lg-6 ">
+                <div className={`${classes.Card} card-small `}>
+                    <div className={`${classes.Card_header} card-header border-bottom`}>
+                        <h6 className="m-0">Mastery</h6>
+                    </div>
+                    <div className="card-body p-0 pb-3 text-center">
+                        <MasteryTable/>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </Hoc>
 );
 
 export default needsView;
---
+++ src/components/NeedsView/ProjectsTable/ProjectsTable.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import classes from './ProjectsTable.module.css';
+import TableActions from "../../shared_components/TableActions/TableActions";
+import Update from "../../shared_components/TableActions/Update/Update";
+import Remove from "../../shared_components/TableActions/Remove/Remove";
+
+const projectsTable = () => (
+    <table className={`${classes.Table} table mb-0`}>
+        <thead className="bg-light">
+        <tr>
+            <th scope="col">Project</th>
+            <th scope="col">Person</th>
+            <th scope="col">Date</th>
+            <th scope="col">Talent Manager</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">Minsk</th>
+            <td>5</td>
+            <td>3</td>
+            <td>L, S, M</td>
+            <td >
+                <TableActions>
+                    <Update/>
+                    <Remove/>
+                </TableActions>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">Brest</th>
+            <td>2</td>
+            <td>0</td>
+            <td>Mastery x2</td>
+            <td >
+                <TableActions>
+                    <Update/>
+                    <Remove/>
+                </TableActions>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">Gomel</th>
+            <td>3</td>
+            <td>1</td>
+            <td>L x2</td>
+            <td >
+                <TableActions>
+                    <Update/>
+                    <Remove/>
+                </TableActions>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">Grodno</th>
+            <td>1</td>
+            <td>1</td>
+            <td></td>
+            <td >
+                <TableActions>
+                    <Update/>
+                    <Remove/>
+                </TableActions>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">Mogilev</th>
+            <td>2</td>
+            <td>0</td>
+            <td>L</td>
+            <td >
+                <TableActions>
+                    <Update/>
+                    <Remove/>
+                </TableActions>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+);
+
+export default projectsTable;
