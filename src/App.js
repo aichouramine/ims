0238273c508d109f9d76247e406881ee977d6cf1
jack.bauer <ignatovich.dm@gmail.com>
2019-08-13 14:36:15 +0300

    Added page transition

--- src/App.js
+++ src/App.js
@@ -9,24 +9,51 @@ import ResourcesNeeds from './containers/ResourcesNeeds/ResourcesNeeds';
 import Dashboard from './containers/Dashboard/Dashboard';
 import FollowUp from './containers/FollowUp/FollowUp';
 import InterviewerProfileContainer from './containers/InterviewerProfileContainer/InterviewerProfileContainer';
-import { Route, Switch } from 'react-router-dom';
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
+import { Route, Switch, withRouter } from 'react-router-dom';
+import './index.css';
+import Sidebar from './components/Navigation/Sidebar/Sidebar'
+import Toolbar from './components/Navigation/Toolbar/Toolbar'
 
 class App extends Component {
+    constructor(props){
+        super(props);
+    }
+
   render() {
+
     return (
       <div>
-        <Layout title="Overview">
-            <Route path="/" exact component={Dashboard}/>
-            <Route path="/interviews" component={InterviewsContainer}/>
-            <Route path="/interviewers" component={InterviewersContainer}/>
-            <Route path="/profile" component={InterviewerProfileContainer}/>
-            <Route exact path="/candidates" component={CandidatesContainer}/>
-            <Route exact path="/candidates/addNewCandidate" component={CandidateCreationContainer}/>
-            <Route exact path="/candidates/details/:id" component={CandidateDetailsContainer}/>
-            <Route path="/needs" component={ResourcesNeeds}/>
-            {/*<Route path="/schedule" component={InterviewsCalendar}/>*/}
-            <Route path="/candidates/followUp/:id" component={FollowUp}/>
-        </Layout>
+          <Sidebar/>
+          <Toolbar/>
+          <Route render={({location}) => (
+              <TransitionGroup>
+                  <CSSTransition
+                      // in={true}
+                      // appear={false}
+                      key={location.key}
+                      classNames={"page-fade"}
+                      timeout={350}
+                      unmountOnExit
+                  >
+                      <Layout title="Overview">
+                          <Switch location={location}>
+                              <Route path="/" exact component={Dashboard}/>
+                              <Route path="/interviews" component={InterviewsContainer}/>
+                              <Route path="/interviewers" component={InterviewersContainer}/>
+                              <Route path="/profile" component={InterviewerProfileContainer}/>
+                              <Route exact path="/candidates" component={CandidatesContainer}/>
+                              <Route exact path="/candidates/addNewCandidate" component={CandidateCreationContainer}/>
+                              <Route exact path="/candidates/details/:id" component={CandidateDetailsContainer}/>
+                              <Route path="/needs" component={ResourcesNeeds}/>
+                              {/*<Route path="/schedule" component={InterviewsCalendar}/>*/}
+                              <Route path="/candidates/followUp/:id" component={FollowUp}/>
+                          </Switch>
+                      </Layout>
+                  </CSSTransition>
+              </TransitionGroup>
+          )}/>
+
       </div>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,9 +5,7 @@ import Sidebar from '../Navigation/Sidebar/Sidebar'
 
 const layout = (props) => (
     <div className={classes.Wrapper}>
-        <Sidebar/>
         <div className="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
-            <Toolbar/>
             <main className={`${classes.Content} main-content-container container-fluid px-5 mb-4`}>
                 {props.children}
             </main>
